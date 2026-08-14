569d0b1129be45864b775c3135d29853365b09bc
sj880428 <sj880428@gmail.com>
2026-08-14 16:03:14 +0900

    Refresh cached app after login ID error update

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'saha-shift-v20260814-8';
+const CACHE_NAME = 'saha-shift-v20260814-9';
 const APP_SHELL = [
   './',
   './index.html',
   './styles.css?v=20260814_4',
   './auth-service.js?v=20260814_3',
-  './app.js?v=20260814_8',
+  './app.js?v=20260814_9',
   './manifest.webmanifest',
   './app-icon.svg'
 ];
