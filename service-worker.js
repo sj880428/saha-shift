e7b4ff772d2fbada118adba92fe16f7f95f283d4
sj880428 <sj880428@gmail.com>
2026-08-14 16:08:18 +0900

    Refresh restored app cache

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'saha-shift-v20260814-9';
+const CACHE_NAME = 'saha-shift-v20260814-10';
 const APP_SHELL = [
   './',
   './index.html',
   './styles.css?v=20260814_4',
   './auth-service.js?v=20260814_3',
-  './app.js?v=20260814_9',
+  './app.js?v=20260814_10',
   './manifest.webmanifest',
   './app-icon.svg'
 ];
