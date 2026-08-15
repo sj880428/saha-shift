f0e6d516735681c2b74a8ed51a0b8fb86a13a078
sj880428 <sj880428@gmail.com>
2026-08-15 15:15:14 +0900

    모바일 생활관 선택 표시 수정: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'saha-shift-v20260815-2';
+const CACHE_NAME = 'saha-shift-v20260815-3';
 const APP_SHELL = [
   './',
   './index.html',
-  './styles.css?v=20260815_2',
+  './styles.css?v=20260815_3',
   './auth-service.js?v=20260814_3',
-  './app.js?v=20260815_2',
+  './app.js?v=20260815_3',
   './manifest.webmanifest',
   './app-icon.svg'
 ];
