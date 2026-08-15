1781e5546722174837b836ce07b26c8506b435cf
sj880428 <sj880428@gmail.com>
2026-08-15 15:09:05 +0900

    아이폰 모바일 화면 세부 개선: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'saha-shift-v20260815-1';
+const CACHE_NAME = 'saha-shift-v20260815-2';
 const APP_SHELL = [
   './',
   './index.html',
-  './styles.css?v=20260815_1',
+  './styles.css?v=20260815_2',
   './auth-service.js?v=20260814_3',
-  './app.js?v=20260815_1',
+  './app.js?v=20260815_2',
   './manifest.webmanifest',
   './app-icon.svg'
 ];
