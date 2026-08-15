8506298a8c8b1852d70469ddd87f9dd0903617fc
sj880428 <sj880428@gmail.com>
2026-08-15 15:29:04 +0900

    로그인 사용자 표시 정리: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'saha-shift-v20260815-3';
+const CACHE_NAME = 'saha-shift-v20260815-4';
 const APP_SHELL = [
   './',
   './index.html',
-  './styles.css?v=20260815_3',
+  './styles.css?v=20260815_4',
   './auth-service.js?v=20260814_3',
-  './app.js?v=20260815_3',
+  './app.js?v=20260815_4',
   './manifest.webmanifest',
   './app-icon.svg'
 ];
