f3556012d2529e6a08f72317f19d67fcc5f7e929
sj880428 <sj880428@gmail.com>
2026-08-19 17:08:18 +0900

    휴무일 연가 차단 캐시 갱신

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'saha-shift-v20260815-5';
+const CACHE_NAME = 'saha-shift-v20260819-1';
 const APP_SHELL = [
   './',
   './index.html',
   './styles.css?v=20260815_5',
   './auth-service.js?v=20260814_3',
-  './app.js?v=20260815_5',
+  './app.js?v=20260819_1',
   './manifest.webmanifest',
   './app-icon.svg'
 ];
